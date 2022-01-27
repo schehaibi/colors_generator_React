7769624652b930f1e2ce5c6178009e6458552725
sahbi chehaibi <schehaibi72@gmail.com>
2022-01-28 00:13:57 +0100

    Updates

---
+++ precache-manifest.f363aba3cd7f587c0c29baaf80138cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f63ee4638fd0ebec2a76d449e93fd53",
+    "url": "/colors_generator_React/index.html"
+  },
+  {
+    "revision": "f3c554f5a6a28bde7ce9",
+    "url": "/colors_generator_React/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "af2b8d77403a28b617f1",
+    "url": "/colors_generator_React/static/js/2.409ae74d.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/colors_generator_React/static/js/2.409ae74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c554f5a6a28bde7ce9",
+    "url": "/colors_generator_React/static/js/main.4b5ad293.chunk.js"
+  },
+  {
+    "revision": "0a3d5aa809082c5144ae",
+    "url": "/colors_generator_React/static/js/runtime-main.b5a2f9e5.js"
+  }
+]);
